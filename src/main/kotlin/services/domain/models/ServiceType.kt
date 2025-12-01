ab650a72a4b27e4deab338a1cb812e692641f687
Jaitovich Bisnaud Jimeneqz <jaitovichbidnaud@gmail.com>
2025-12-01 17:40:35 -0600

    Agregando archivo del proyecto: src\main\kotlin\services\domain\models\ServiceType.kt

---
+++ src/main/kotlin/services/domain/models/ServiceType.kt
@@ -0,0 +1,12 @@
+package com.example.services.domain.models
+
+/**
+ * ServiceType Domain Model
+ * Representa un tipo de servicio que ofrece la clínica veterinaria
+ */
+data class ServiceType(
+    val id: Int,
+    val name: String
+)
+
+
