cad03c0bede4ef17e32675025ae36c0b25161ba1
Jaitovich Bisnaud Jimeneqz <jaitovichbidnaud@gmail.com>
2025-12-01 17:40:28 -0600

    Agregando archivo del proyecto: src\main\kotlin\auth\infrastructure\persistence\database\VeterinariansTable.kt

---
+++ src/main/kotlin/auth/infrastructure/persistence/database/VeterinariansTable.kt
@@ -0,0 +1,13 @@
+package com.example.auth.infrastructure.persistence.database
+
+import org.jetbrains.exposed.dao.id.IntIdTable
+import org.jetbrains.exposed.sql.kotlin.datetime.datetime
+
+object VeterinariansTable : IntIdTable("veterinarians") {
+    val userId = integer("user_id").uniqueIndex()
+    val fullName = varchar("fullname", 255)
+    val phone = varchar("phone", 20).nullable()
+    val createdAt = datetime("created_at")
+    val updatedAt = datetime("updated_at")
+}
+
