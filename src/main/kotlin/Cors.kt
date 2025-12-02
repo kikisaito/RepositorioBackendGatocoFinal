bb0cff59eabf2dd4a1dfea1be7fc51123662fd67
Jaitovich Bisnaud Jimeneqz <jaitovichbidnaud@gmail.com>
2025-12-01 19:24:05 -0600

    Arreglando CORS final

--- src/main/kotlin/Cors.kt
+++ src/main/kotlin/Cors.kt
@@ -7,21 +7,27 @@ import io.ktor.server.plugins.cors.routing.*
 
 fun Application.configureCORS() {
     install(CORS) {
+        // Métodos permitidos (Verbos HTTP)
         allowMethod(HttpMethod.Options)
         allowMethod(HttpMethod.Post)
         allowMethod(HttpMethod.Get)
         allowMethod(HttpMethod.Put)
         allowMethod(HttpMethod.Delete)
+        allowMethod(HttpMethod.Patch)
+
+        // Cabeceras permitidas
         allowHeader(HttpHeaders.ContentType)
-        allowHeader("Authorization")
-        exposeHeader("Authorization")
-        
-        // Permitir Angular localhost y 127.0.0.1
-        allowHost("localhost:4200")
-        allowHost("127.0.0.1:4200")
-        
+        allowHeader(HttpHeaders.Authorization)
+        allowHeader(HttpHeaders.AccessControlAllowOrigin)
+        exposeHeader(HttpHeaders.Authorization)
+
+        // --- EL CAMBIO IMPORTANTE ---
+        // Quitamos "allowHost(localhost)" y ponemos anyHost()
+        // Esto permite que tu S3 (y cualquiera) pueda conectarse.
+        anyHost()
+
+        // Configuración de credenciales
         allowCredentials = true
         maxAgeInSeconds = 3600
     }
 }
-
